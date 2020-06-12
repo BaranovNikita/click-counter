b8c2d7d96ecdd2ce63c6060402efe24ea38d3fbc
Никита Баранов <baranov.nikita@gmail.com>
2020-06-13 01:07:19 +0300

    first green test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
-import { render } from '@testing-library/react';
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders without error', () => {
-
+  const wrapper = shallow(<App />)
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1)
 })
 test('renders increment button', () => {
 
